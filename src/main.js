3db621355782cb745eaa5f543498d73b9ec02217
boilingSugar <1418068612@qq.com>
2021-12-02 14:09:33 +0800

    type;fix title: 安装依赖，引入less等工具

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/css/common.less'   // 常用样式
+import './assets/css/index.less'    // 初始样式
 import './index.css'
+// import router from './router'
 
-createApp(App).mount('#app')
+
+
+const app = createApp(App)
+
+app.mount('#app')
